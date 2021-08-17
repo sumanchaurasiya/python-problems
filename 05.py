c6a2813cef14ac017b558b56ccb1f89c55a9cbf9
sumanchaurasiya <75887504+sumanchaurasiya@users.noreply.github.com>
2021-08-17 20:55:27 +0530

    day 3 problem

---
+++ 05.py
@@ -0,0 +1,5 @@
+# convert celsius to fahrenheit
+
+celsius = float(input("Enter celsicus temperature "))
+temp = (celsius*(9/5)+32) 
+print("Fahrenheit temperature is : " + str(temp))
---
+++ 06.py
@@ -0,0 +1,8 @@
+import calendar
+
+# yy = int(input("Enter Year : "))
+# mm = int(input("Enter month : "))
+
+# print(calendar.month(yy,mm))
+
+print(calendar.calendar(2021,2,2,6))
