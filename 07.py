0f4bbbe7b66dd85dd3213ac51c3b0022144088ec
sumanchaurasiya <75887504+sumanchaurasiya@users.noreply.github.com>
2021-08-19 20:52:18 +0530

    day 4 problems

---
+++ 07.py
@@ -0,0 +1,11 @@
+# program to print length of any string given by user
+
+'''name = input("Enter name : ")
+l = len(name)
+print(l)'''
+
+# make a tamplat in which user gives his name and college and we get combination of string.
+
+name = input("Enter your name : ")
+clg = input("Enter your college name : ")
+print(name + ", " + clg)
---
+++ 08.py
@@ -0,0 +1,7 @@
+# program to convert decimal to other number system
+num = int(input("Enter number : "))
+
+print("Decimal number is " + str(num))
+print("Binary number is " + str(bin(num)))
+print("Octal number is " + str(oct(num)))
+print("Hexadecimal number is " + str(hex(num)))
