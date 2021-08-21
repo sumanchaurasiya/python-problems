2e44d99a034b98e0aeb967f46338dfaca93830ba
sumanchaurasiya <75887504+sumanchaurasiya@users.noreply.github.com>
2021-08-21 21:30:52 +0530

    day5 problems

---
+++ 09.py
@@ -0,0 +1,6 @@
+# how would you confirm that 2 strings have same identity?
+
+name1 = input("Enter name1 : ")
+name2 = input("Enter name2 : ")
+
+print(name1==(name2))
---
+++ 10.py
@@ -0,0 +1,5 @@
+# split a string on a specific character ('')
+
+para = input("Enter paragraph : ")
+result = para.split(' ')
+print(result)
