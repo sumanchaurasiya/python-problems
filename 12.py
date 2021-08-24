c07b1f2b269bb658c0ec1504c9e8fe4c23018316
sumanchaurasiya <75887504+sumanchaurasiya@users.noreply.github.com>
2021-08-24 20:31:12 +0530

    day7 problems

---
+++ 12.py
@@ -0,0 +1,10 @@
+# store 5 items in the list and count occurance of any item
+
+l1 = input("Enter fruit1 : ")
+l2 = input("Enter fruit2 : ")
+l3 = input("Enter fruit3 : ")
+l4 = input("Enter fruit4 : ")
+l5 = input("Enter fruit5 : ")
+
+listFruit = [l1, l2, l3, l4, l5]
+print(listFruit.count("mango"))
---
+++ 13.py
@@ -0,0 +1,15 @@
+# reverse tuple 
+
+'''t1 = (3, 5, 6)
+y = reversed(t1)
+print(tuple(y))'''
+
+# length of tuple 
+'''t1 = (5, 7,9,7)
+print(len(t1))'''
+
+# find 4 th element and last 4th element of ttuple
+t1 = (5,8,90,6,45,78,23,65,56,31,11)
+# print(t1[3] , t1[-4])
+print(t1[2:6])
+
